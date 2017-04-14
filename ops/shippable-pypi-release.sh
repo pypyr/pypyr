21b4180519ce1b72433fec4e3f5186885f386416
345thomas <thomas@345.systems>
2017-04-14 16:08:12 +0100

    get rid of virtual environments in shippable entirely for christ's sake

--- ops/shippable-pypi-release.sh
+++ ops/shippable-pypi-release.sh
@@ -4,34 +4,6 @@
 # stop processing on any statement return != 0
 set -o errexit
 
-# take one parameter: name of virtual environment
-create_virtualenv()
-{
-  echo "Creating virtual environment name is: ${1}"
-  mkdir -p $HOME/${1}/
-  virtualenv -p $SHIPPABLE_PYTHON $HOME/${1}/
-  # virtualenv activate doesn't work with strict no unset vars mode
-  # set +u
-  . $HOME/${1}/bin/activate
-  # set -u
-}
-
-# take one parameter: name of virtual environment
-remove_virtualenv()
-{
-  echo "removing virtual env ${1} if it exists."
-  if [ -n "${VIRTUAL_ENV-}" ] && [ "`basename "${VIRTUAL_ENV}"`" = "$HOME/${1}/" ]; then
-    # get rid of the virtual env
-    echo "Virtual Environment name is: ${VIRTUAL_ENV}"
-    echo "Removing virtualenv ${1}"
-    deactivate
-    rm -rf $HOME/$1
-  fi
-}
-
-# do packaging inside a virtual env so this doesn't pollute global python scope.
-create_virtualenv .deployenv
-
 # Use deploy switch on setup.py to install deploy deps.
 pip install -e .[deploy]
 
@@ -49,12 +21,11 @@ twine upload --repository-url ${PYPI_URL} --username ${PYPI_USERNAME} --password
 echo "----------Done with twine upload-----------------------------------------"
 
 # all done, clean-up
-remove_virtualenv .deployenv
+pip uninstall pypyr
 
 # smoke test
 echo "----------Deploy to pypi complete. Testing in new virtual env.-----------"
 
-create_virtualenv .pypitest
 pip install pypyr
 # pypyr --v will return "pypyr x.y.z" - get everything after the space for the
 # bare version number.
@@ -64,5 +35,3 @@ if [ "${TEST_DEPLOY_VERSION}" =  "${NEW_VERSION}" ]; then
 else
   echo "Something went wrong. Deployed version is ${TEST_DEPLOY_VERSION}, but expected ${NEW_VERSION}"
 fi;
-
-remove_virtualenv .pypitest
