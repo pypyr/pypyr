966a2b744d6b053c37efc8d20941f0aee34ad76e
345thomas <thomas@345.systems>
2017-04-14 18:42:25 +0100

    amend branch check to dev for test

--- ops/shippable-pypi-release.sh
+++ ops/shippable-pypi-release.sh
@@ -4,48 +4,24 @@
 # stop processing on any statement return != 0
 set -o errexit
 
-# Use deploy switch on setup.py to install deploy deps.
-pip install -e .[deploy]
+# Build wheel in dist/
+python setup.py bdist_wheel
 
+# Deploy wheel
+twine upload --repository-url ${PYPI_URL} --username ${PYPI_USERNAME} --password ${PYPI_PASSWORD} dist/pypyr-${NEW_VERSION}-py3-none-any.whl
+
+echo "----------Done with twine upload-------------------------------------"
+
+# smoke test
+echo "----------Deploy to pypi complete. Testing in new virtual env.-------"
+
+pip install pypyr
 # pypyr --v will return "pypyr x.y.z" - get everything after the space for the
 # bare version number.
-NEW_VERSION=`pypyr --v | cut -d " " -f2`
-echo "New version is: ${NEW_VERSION}"
-
-TAG_NAME="v${NEW_VERSION}"
-
-if [ $(git tag -l "${TAG_NAME}") ]; then
-    echo "----------tag already exists.----------------------------------------"
-    return 0
+TEST_DEPLOY_VERSION=`pypyr --v | cut -d " " -f2`
+if [ "${TEST_DEPLOY_VERSION}" =  "${NEW_VERSION}" ]; then
+  echo "Deployed version is ${TEST_DEPLOY_VERSION}. Smoke test passed OK."
 else
-    echo "version tag doesn't exist. create tag. ${TAG_NAME}"
-
-    # Build wheel in dist/
-    python setup.py bdist_wheel
-
-    # Deploy wheel
-    twine upload --repository-url ${PYPI_URL} --username ${PYPI_USERNAME} --password ${PYPI_PASSWORD} dist/pypyr-${NEW_VERSION}-py3-none-any.whl
-
-    git tag "${TAG_NAME}"
-    git push origin master --tags
-
-    echo "----------Done with twine upload-------------------------------------"
-
-    # all done, clean-up
-    pip uninstall pypyr
-
-    # smoke test
-    echo "----------Deploy to pypi complete. Testing in new virtual env.-------"
-
-    pip install pypyr
-    # pypyr --v will return "pypyr x.y.z" - get everything after the space for the
-    # bare version number.
-    TEST_DEPLOY_VERSION=`pypyr --v | cut -d " " -f2`
-    if [ "${TEST_DEPLOY_VERSION}" =  "${NEW_VERSION}" ]; then
-      echo "Deployed version is ${TEST_DEPLOY_VERSION}. Smoke test passed OK."
-    else
-      echo "Something went wrong. Deployed version is ${TEST_DEPLOY_VERSION}, but expected ${NEW_VERSION}"
-      return 1
-    fi;
-
-fi
+  echo "Something went wrong. Deployed version is ${TEST_DEPLOY_VERSION}, but expected ${NEW_VERSION}"
+  return 1
+fi;
---
+++ ops/shippable-tag-release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Runs on shippable. tags branchs as it stands and pushes tags to github.
+
+# stop processing on any statement return != 0
+set -o errexit
+
+# Use deploy switch on setup.py to install deploy deps.
+pip install -e .[deploy]
+
+# pypyr --v will return "pypyr x.y.z" - get everything after the space for the
+# bare version number.
+NEW_VERSION=`pypyr --v | cut -d " " -f2`
+echo "New version is: ${NEW_VERSION}"
+TAG_NAME="v${NEW_VERSION}"
+
+# all done, clean-up
+pip uninstall pypyr
+
+if [ $(git tag -l "${TAG_NAME}") ]; then
+    echo "----------tag already exists.----------------------------------------"
+    return 0
+else
+    echo "version tag doesn't exist. create tag. ${TAG_NAME}"
+    git tag "${TAG_NAME}"
+    git push origin --tags
+fi;
