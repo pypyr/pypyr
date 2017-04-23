cf7031e52249a1fc9baa75faf910d4ae4e6490e8
345thomas <thomas@345.systems>
2017-04-23 01:30:13 +0100

    make echo repr for unicode compat

--- pypyr/steps/echo.py
+++ pypyr/steps/echo.py
@@ -19,6 +19,6 @@ def run_step(context):
     assert context, ("context must be set for echo. Did you set "
                      "--context 'echoMe=text here'?")
 
-    logger.info(context.get_formatted('echoMe'))
+    logger.info(context.get_formatted_string(repr(context['echoMe'])))
 
     logger.debug("done")
