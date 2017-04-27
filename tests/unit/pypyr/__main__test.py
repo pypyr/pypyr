1e057fe49552a3c124de3333960abe8a2e61aecf
345thomas <thomas@345.systems>
2017-04-27 18:47:06 +0100

    unit tests for cli

---
+++ tests/unit/pypyr/__main__test.py
@@ -0,0 +1,11 @@
+"""__main__.py unit tests."""
+import pypyr.__main__
+from unittest.mock import patch
+
+
+def test_main_calls_main():
+    """The python -m entry point should call pypyr.cli.main()"""
+    with patch('pypyr.cli.main') as mock_cli_main:
+        pypyr.__main__.main()
+
+    mock_cli_main.assert_called_once()
