fbaa15032a03768ee50a1460b0e60e4604a5714f
345thomas <thomas@345.systems>
2017-05-29 23:29:23 +0100

    poll wait_until_true

---
+++ pypyr/utils/poll.py
@@ -0,0 +1,42 @@
+"""Utility functions for polling."""
+import time
+import logging
+
+# pypyr logger means the log level will be set correctly and output formatted.
+logger = logging.getLogger(__name__)
+
+
+def wait_until_true(interval, max_attempts):
+    """Decorator that executes a function until it returns True.
+
+    Executes wrapped function at every number of seconds specified by interval,
+    until wrapped function either returns True or max_attempts are exhausted,
+    whichever comes 1st.
+
+    Args:
+        interval: In seconds. How long to wait between executing the wrapped
+                  function.
+        max_attempts: int. Execute wrapped function up to this limit.
+
+    Returns:
+        Bool. True if wrapped function returned True. False if reached
+              max_attempts without the wrapped function ever returning True.
+    """
+    def decorator(f):
+        logger.debug("started")
+
+        def sleep_looper(*args, **kwargs):
+            logger.debug(f"Looping every {interval} seconds for "
+                         f"{max_attempts} attempts")
+            for i in range(1, max_attempts + 1):
+                result = f(*args, **kwargs)
+                if result:
+                    logger.debug(f"iteration {i}. Desired state reached.")
+                    return True
+                logger.debug(f"iteration {i}. Still waiting. . .")
+                time.sleep(interval)
+            return False
+        return sleep_looper
+
+    logger.debug("done")
+    return decorator
---
+++ tests/unit/pypyr/utils/poll_test.py
@@ -0,0 +1,90 @@
+"""poll.py unit tests."""
+import pypyr.utils.poll as poll
+from unittest.mock import MagicMock
+
+
+# ----------------- wait_until_true -------------------------------------------
+def test_wait_until_true_with_static_decorator():
+    """wait_until_true with static decorator"""
+    mock = MagicMock()
+    mock.side_effect = [
+        'test string 1',
+        'test string 2',
+        'test string 3',
+        'expected value',
+        'test string 5'
+    ]
+
+    @poll.wait_until_true(interval=0.1, max_attempts=10)
+    def decorate_me(arg1, arg2):
+        """Test static decorator syntax"""
+        assert arg1 == 'v1'
+        assert arg2 == 'v2'
+        if mock(arg1) == 'expected value':
+            return True
+        else:
+            return False
+
+    assert decorate_me('v1', 'v2')
+    assert mock.call_count == 4
+    mock.assert_called_with('v1')
+
+
+def test_wait_until_true_invoke_inline():
+    """wait_until_true with dynamic invocation."""
+    mock = MagicMock()
+    mock.side_effect = [
+        'test string 1',
+        'test string 2',
+        'test string 3',
+        'expected value',
+        'test string 5'
+    ]
+
+    def decorate_me(arg1, arg2):
+        """Test static decorator syntax"""
+        assert arg1 == 'v1'
+        assert arg2 == 'v2'
+        if mock(arg1) == 'expected value':
+            return True
+        else:
+            return False
+
+    assert poll.wait_until_true(interval=0.1, max_attempts=10)(
+        decorate_me)('v1', 'v2')
+    assert mock.call_count == 4
+    mock.assert_called_with('v1')
+
+
+def test_wait_until_true_with_timeout():
+    """wait_until_true with dynamic invocation, exhaust wait attempts."""
+    mock = MagicMock()
+    mock.side_effect = [
+        'test string 1',
+        'test string 2',
+        'test string 3',
+        'test string 4',
+        'test string 5',
+        'test string 6',
+        'test string 7',
+        'test string 8',
+        'test string 9',
+        'test string 10',
+        'test string 11',
+    ]
+
+    def decorate_me(arg1, arg2):
+        """Test static decorator syntax"""
+        assert arg1 == 'v1'
+        assert arg2 == 'v2'
+        if mock(arg1) == 'expected value':
+            return True
+        else:
+            return False
+
+    assert not poll.wait_until_true(interval=0.1, max_attempts=10)(
+        decorate_me)('v1', 'v2')
+    assert mock.call_count == 10
+    mock.assert_called_with('v1')
+
+# ----------------- wait_until_true -------------------------------------------
