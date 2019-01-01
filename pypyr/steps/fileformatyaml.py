87c3cdf7a8ea9c35b9f05ef87f3cdedd51f622c5
345thomas <thomas@345.systems>
2019-01-01 19:51:43 +0000

    fileformatyaml new context input format

--- pypyr/steps/fileformatyaml.py
+++ pypyr/steps/fileformatyaml.py
@@ -1,6 +1,7 @@
 """pypyr step that parses yaml for string substitutions, writes output."""
 import logging
-from pypyr.utils.filesystem import ObjectRewriter, YamlRepresenter
+from pypyr.steps.dsl.fileinoutrewriter import ObjectRewriterStep
+from pypyr.utils.filesystem import YamlRepresenter
 
 # logger means the log level will be set correctly
 logger = logging.getLogger(__name__)
@@ -13,34 +14,57 @@ def run_step(context):
 
     Args:
         context: pypyr.context.Context. Mandatory.
-                 The following context keys expected:
-                - fileFormatYamlIn. mandatory. path-like.
-                  Path to source file on disk.
-                - fileFormatYamlOut. mandatory. path-like. Write output file to
-                  here. Will create directories in path for you.
+        - fileFormatYaml
+            - in. mandatory.
+              str, path-like, or an iterable (list/tuple) of
+              strings/paths. Each str/path can be a glob, relative or
+              absolute path.
+            - out. optional. path-like.
+              Can refer to a file or a directory.
+              will create directory structure if it doesn't exist. If
+              in-path refers to >1 file (e.g it's a glob or list), out
+              path can only be a directory - it doesn't make sense to
+              write >1 file to the same single file (this is not an
+              appender.) To ensure out_path is read as a directory and
+              not a file, be sure to have the path separator (/) at the
+              end.
+              If out_path is not specified or None, will in-place edit
+              and overwrite the in-files.
 
     Returns:
         None.
 
     Raises:
         FileNotFoundError: take a guess
-        pypyr.errors.KeyNotInContextError: fileFormatYamlIn or
-            fileFormatYamlOut missing in context.
-        pypyr.errors.KeyInContextHasNoValueError: fileFormatYamlIn or
-            fileFormatYamlOut exists but is None.
+        pypyr.errors.KeyNotInContextError: fileFormatYaml or
+            fileFormatYaml['in'] missing in context.
+        pypyr.errors.KeyInContextHasNoValueError: fileFormatYaml or
+            fileFormatYaml['in'] exists but is None.
 
     """
     logger.debug("started")
-    context.assert_keys_have_values(__name__,
-                                    'fileFormatYamlIn',
-                                    'fileFormatYamlOut')
 
-    in_path = context.get_formatted('fileFormatYamlIn')
-    out_path = context.get_formatted(
-        'fileFormatYamlOut') if 'fileFormatYamlOut' in context else None
+    deprecated(context)
 
-    rewriter = ObjectRewriter(context.get_formatted_iterable,
-                              YamlRepresenter())
-    rewriter.files_in_to_out(in_path=in_path, out_path=out_path)
+    ObjectRewriterStep(__name__, 'fileFormatYaml', context).run_step(
+        YamlRepresenter())
 
     logger.debug("done")
+
+
+def deprecated(context):
+    """Create new style in params from deprecated."""
+    if 'fileFormatYamlIn' in context:
+        context.assert_keys_have_values(__name__,
+                                        'fileFormatYamlIn',
+                                        'fileFormatYamlOut')
+
+        context['fileFormatYaml'] = {'in': context['fileFormatYamlIn'],
+                                     'out': context['fileFormatYamlOut']}
+
+        logger.warning("fileFormatYamlIn and fileFormatYamlOut "
+                       "are deprecated. They will stop working upon the next "
+                       "major release. Use the new context key fileFormatYaml "
+                       "instead. It's a lot better, promise! For the moment "
+                       "pypyr is creating the new fileFormatYaml key for you "
+                       "under the hood.")
--- tests/unit/pypyr/steps/fileformatyaml_test.py
+++ tests/unit/pypyr/steps/fileformatyaml_test.py
@@ -1,10 +1,13 @@
 """fileformatyaml.py unit tests."""
+import logging
 import os
+import shutil
+from unittest.mock import patch
+import ruamel.yaml as yaml
 from pypyr.context import Context
 from pypyr.errors import KeyInContextHasNoValueError, KeyNotInContextError
 import pypyr.steps.fileformatyaml as fileformat
 import pytest
-import ruamel.yaml as yaml
 
 
 def test_fileformatyaml_no_inpath_raises():
@@ -15,67 +18,40 @@ def test_fileformatyaml_no_inpath_raises():
     with pytest.raises(KeyNotInContextError) as err_info:
         fileformat.run_step(context)
 
-    assert str(err_info.value) == ("context['fileFormatYamlIn'] "
-                                   "doesn't exist. It must exist for "
-                                   "pypyr.steps.fileformatyaml.")
+    assert str(err_info.value) == (
+        "fileFormatYaml not found in the pypyr context.")
 
 
 def test_fileformatyaml_empty_inpath_raises():
     """Empty in path raises."""
     context = Context({
-        'fileFormatYamlIn': None})
+        'fileFormatYaml': {'in': None}})
 
     with pytest.raises(KeyInContextHasNoValueError) as err_info:
         fileformat.run_step(context)
 
-    assert str(err_info.value) == ("context['fileFormatYamlIn'] must have "
-                                   "a value for pypyr.steps.fileformatyaml.")
-
-
-def test_fileformatyaml_no_outpath_raises():
-    """None out path raises."""
-    context = Context({
-        'fileFormatYamlIn': 'blah',
-        'k1': 'v1'})
-
-    with pytest.raises(KeyNotInContextError) as err_info:
-        fileformat.run_step(context)
-
-    assert str(err_info.value) == ("context['fileFormatYamlOut'] "
-                                   "doesn't exist. It must exist for "
-                                   "pypyr.steps.fileformatyaml.")
-
-
-def test_fileformatyaml_empty_outpath_raises():
-    """Empty in path raises."""
-    context = Context({
-        'fileFormatYamlIn': 'blah',
-        'fileFormatYamlOut': None})
-
-    with pytest.raises(KeyInContextHasNoValueError) as err_info:
-        fileformat.run_step(context)
-
-    assert str(err_info.value) == ("context['fileFormatYamlOut'] must have "
+    assert str(err_info.value) == ("context['fileFormatYaml']['in'] must have "
                                    "a value for pypyr.steps.fileformatyaml.")
 
 
 def test_fileformatyaml_pass_no_substitutions():
     """Relative path to file should succeed.
 
-     Strictly speaking not a unit test.
+    Strictly speaking not a unit test.
     """
     context = Context({
         'ok1': 'ov1',
-        'fileFormatYamlIn': './tests/testfiles/test.yaml',
-        'fileFormatYamlOut': './tests/testfiles/out/out.yaml'})
+        'fileFormatYaml': {'in': './tests/testfiles/test.yaml',
+                           'out': './tests/testfiles/out/out.yaml'}})
 
     fileformat.run_step(context)
 
     assert context, "context shouldn't be None"
-    assert len(context) == 3, "context should have 2 items"
+    assert len(context) == 2, "context should have 2 items"
     assert context['ok1'] == 'ov1'
-    assert context['fileFormatYamlIn'] == './tests/testfiles/test.yaml'
-    assert context['fileFormatYamlOut'] == './tests/testfiles/out/out.yaml'
+    assert context['fileFormatYaml'] == {
+        'in': './tests/testfiles/test.yaml',
+        'out': './tests/testfiles/out/out.yaml'}
 
     with open('./tests/testfiles/out/out.yaml') as outfile:
         yaml_loader = yaml.YAML(typ='rt', pure=True)
@@ -100,7 +76,7 @@ def test_fileformatyaml_pass_no_substitutions():
 def test_fileformatyaml_pass_with_substitutions():
     """Relative path to file should succeed.
 
-     Strictly speaking not a unit test.
+    Strictly speaking not a unit test.
     """
     context = Context({
         'k1': 'v1',
@@ -108,17 +84,17 @@ def test_fileformatyaml_pass_with_substitutions():
         'k3': 'v3',
         'k4': 'v4',
         'k5': 'v5',
-        'fileFormatYamlIn': './tests/testfiles/testsubst.yaml',
-        'fileFormatYamlOut': './tests/testfiles/out/outsubst.yaml'})
+        'fileFormatYaml': {'in': './tests/testfiles/testsubst.yaml',
+                           'out': './tests/testfiles/out/outsubst.yaml'}})
 
     fileformat.run_step(context)
 
     assert context, "context shouldn't be None"
-    assert len(context) == 7, "context should have 7 items"
+    assert len(context) == 6, "context should have 6 items"
     assert context['k1'] == 'v1'
-    assert context['fileFormatYamlIn'] == './tests/testfiles/testsubst.yaml'
-    assert context['fileFormatYamlOut'] == ('./tests/testfiles/out/'
-                                            'outsubst.yaml')
+    assert context['fileFormatYaml'] == {
+        'in': './tests/testfiles/testsubst.yaml',
+        'out': './tests/testfiles/out/outsubst.yaml'}
 
     with open('./tests/testfiles/out/outsubst.yaml') as outfile:
         yaml_loader = yaml.YAML(typ='rt', pure=True)
@@ -146,10 +122,113 @@ def test_fileformatyaml_pass_with_substitutions():
     os.remove('./tests/testfiles/out/outsubst.yaml')
 
 
+def test_fileformatyaml_edit_with_substitutions():
+    """Relative path to file should succeed, with no out meaning edit.
+
+    Strictly speaking not a unit test.
+    """
+    shutil.copyfile('./tests/testfiles/testsubst.yaml',
+                    './tests/testfiles/out/edittestsubst.yaml')
+
+    context = Context({
+        'k1': 'v1',
+        'k2': 'v2',
+        'k3': 'v3',
+        'k4': 'v4',
+        'k5': 'v5',
+        'fileFormatYaml': {'in': './tests/testfiles/out/edittestsubst.yaml'}})
+
+    fileformat.run_step(context)
+
+    assert context, "context shouldn't be None"
+    assert len(context) == 6, "context should have 6 items"
+    assert context['k1'] == 'v1'
+    assert context['fileFormatYaml'] == {
+        'in': './tests/testfiles/out/edittestsubst.yaml'}
+
+    with open('./tests/testfiles/out/edittestsubst.yaml') as outfile:
+        yaml_loader = yaml.YAML(typ='rt', pure=True)
+        outcontents = yaml_loader.load(outfile)
+
+    expected = {
+        'key': 'v1value1 !£$%# *',
+        'key2v2': 'blah',
+        # there is a comment here
+        'key3': [
+            'l1',
+            # and another
+            '!£$% * v3',
+            'l2', ['l31v4',
+                   {'l32': ['l321',
+                            'l322v5']
+                    }
+                   ]
+        ]
+    }
+
+    assert outcontents == expected
+
+    # atrociously lazy test clean-up
+    os.remove('./tests/testfiles/out/edittestsubst.yaml')
+
+
 def test_fileformatyaml_pass_with_path_substitutions():
     """Relative path to file should succeed with path subsitutions.
 
-     Strictly speaking not a unit test.
+    Strictly speaking not a unit test.
+    """
+    context = Context({
+        'k1': 'v1',
+        'k2': 'v2',
+        'k3': 'v3',
+        'k4': 'v4',
+        'k5': 'v5',
+        'pathIn': 'testsubst',
+        'pathOut': 'outsubst',
+        'fileFormatYaml': {'in': './tests/testfiles/{pathIn}.yaml',
+                           'out': './tests/testfiles/out/{pathOut}.yaml'}})
+
+    fileformat.run_step(context)
+
+    assert context, "context shouldn't be None"
+    assert len(context) == 8, "context should have 8 items"
+    assert context['k1'] == 'v1'
+    assert context['fileFormatYaml'] == {
+        'in': './tests/testfiles/{pathIn}.yaml',
+        'out': './tests/testfiles/out/{pathOut}.yaml'}
+
+    with open('./tests/testfiles/out/outsubst.yaml') as outfile:
+        yaml_loader = yaml.YAML(typ='rt', pure=True)
+        outcontents = yaml_loader.load(outfile)
+
+    expected = {
+        'key': 'v1value1 !£$%# *',
+        'key2v2': 'blah',
+        # there is a comment here
+        'key3': [
+            'l1',
+            # and another
+            '!£$% * v3',
+            'l2', ['l31v4',
+                   {'l32': ['l321',
+                            'l322v5']
+                    }
+                   ]
+        ]
+    }
+
+    assert outcontents == expected
+
+    # atrociously lazy test clean-up
+    os.remove('./tests/testfiles/out/outsubst.yaml')
+
+# --------------------------- deprecated -------------------------------------
+
+
+def test_fileformatyaml_pass_with_path_substitutions_deprecated():
+    """Relative path to file should succeed with path subsitutions.
+
+    Strictly speaking not a unit test.
     """
     context = Context({
         'k1': 'v1',
@@ -162,14 +241,27 @@ def test_fileformatyaml_pass_with_path_substitutions():
         'fileFormatYamlIn': './tests/testfiles/{pathIn}.yaml',
         'fileFormatYamlOut': './tests/testfiles/out/{pathOut}.yaml'})
 
-    fileformat.run_step(context)
+    logger = logging.getLogger('pypyr.steps.fileformatyaml')
+    with patch.object(logger, 'warning') as mock_logger_warn:
+        fileformat.run_step(context)
+
+    mock_logger_warn.assert_called_once_with(
+        "fileFormatYamlIn and fileFormatYamlOut are deprecated. "
+        "They will stop "
+        "working upon the next major release. Use the new context key "
+        "fileFormatYaml instead. It's a lot better, promise! "
+        "For the moment pypyr "
+        "is creating the new fileFormatYaml key for you under the hood.")
 
     assert context, "context shouldn't be None"
-    assert len(context) == 9, "context should have 9 items"
+    assert len(context) == 10, "context should have 10 items"
     assert context['k1'] == 'v1'
     assert context['fileFormatYamlIn'] == './tests/testfiles/{pathIn}.yaml'
     assert context['fileFormatYamlOut'] == ('./tests/testfiles/out/'
                                             '{pathOut}.yaml')
+    assert context['fileFormatYaml'] == {
+        'in': './tests/testfiles/{pathIn}.yaml',
+        'out': './tests/testfiles/out/{pathOut}.yaml'}
 
     with open('./tests/testfiles/out/outsubst.yaml') as outfile:
         yaml_loader = yaml.YAML(typ='rt', pure=True)
@@ -197,6 +289,34 @@ def test_fileformatyaml_pass_with_path_substitutions():
     os.remove('./tests/testfiles/out/outsubst.yaml')
 
 
+def test_fileformatyaml_no_outpath_raises_deprecated():
+    """None out path raises."""
+    context = Context({
+        'fileFormatYamlIn': 'blah',
+        'k1': 'v1'})
+
+    with pytest.raises(KeyNotInContextError) as err_info:
+        fileformat.run_step(context)
+
+    assert str(err_info.value) == ("context['fileFormatYamlOut'] "
+                                   "doesn't exist. It must exist for "
+                                   "pypyr.steps.fileformatyaml.")
+
+
+def test_fileformatyaml_empty_outpath_raises_deprecated():
+    """Empty in path raises."""
+    context = Context({
+        'fileFormatYamlIn': 'blah',
+        'fileFormatYamlOut': None})
+
+    with pytest.raises(KeyInContextHasNoValueError) as err_info:
+        fileformat.run_step(context)
+
+    assert str(err_info.value) == ("context['fileFormatYamlOut'] must have "
+                                   "a value for pypyr.steps.fileformatyaml.")
+# --------------------------- teardown ---------------------------------------
+
+
 def teardown_module(module):
-    """Teardown"""
+    """Teardown."""
     os.rmdir('./tests/testfiles/out/')
