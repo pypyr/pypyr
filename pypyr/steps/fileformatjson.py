f5f942609cc0e39c6e7a46263f7f979fb10fe8cd
345thomas <thomas@345.systems>
2019-01-01 19:15:49 +0000

    fileformatjson new input context with deprecation support.

--- pypyr/steps/fileformatjson.py
+++ pypyr/steps/fileformatjson.py
@@ -1,6 +1,7 @@
 """pypyr step that parses json file for string substitutions, writes output."""
 import logging
-from pypyr.utils.filesystem import ObjectRewriter, JsonRepresenter
+from pypyr.steps.dsl.fileinoutrewriter import ObjectRewriterStep
+from pypyr.utils.filesystem import JsonRepresenter
 
 # logger means the log level will be set correctly
 logger = logging.getLogger(__name__)
@@ -13,35 +14,57 @@ def run_step(context):
 
     Args:
         context: pypyr.context.Context. Mandatory.
-                 The following context keys expected:
-                - fileFormatJsonIn. mandatory. path-like.
-                  Path to source file on disk.
-                - fileFormatJsonOut. mandatory. path-like. Write output file to
-                  here. Will create directories in path for you.
+                - fileFormatJson
+                    - in. mandatory.
+                      str, path-like, or an iterable (list/tuple) of
+                      strings/paths. Each str/path can be a glob, relative or
+                      absolute path.
+                    - out. optional. path-like.
+                      Can refer to a file or a directory.
+                      will create directory structure if it doesn't exist. If
+                      in-path refers to >1 file (e.g it's a glob or list), out
+                      path can only be a directory - it doesn't make sense to
+                      write >1 file to the same single file (this is not an
+                      appender.) To ensure out_path is read as a directory and
+                      not a file, be sure to have the path separator (/) at the
+                      end.
+                      If out_path is not specified or None, will in-place edit
+                      and overwrite the in-files.
 
     Returns:
         None.
 
     Raises:
         FileNotFoundError: take a guess
-        pypyr.errors.KeyNotInContextError: fileFormatJsonIn or
-            fileFormatJsonOut missing in context.
-        pypyr.errors.KeyInContextHasNoValueError: fileFormatJsonIn or
-            fileFormatJsonOut exists but is None.
+        pypyr.errors.KeyNotInContextError: fileFormatJson or
+            fileFormatJson['in'] missing in context.
+        pypyr.errors.KeyInContextHasNoValueError: fileFormatJson or
+            fileFormatJson['in'] exists but is None.
 
     """
     logger.debug("started")
-    context.assert_keys_have_values(__name__,
-                                    'fileFormatJsonIn',
-                                    'fileFormatJsonOut')
 
-    in_path = context.get_formatted('fileFormatJsonIn')
+    deprecated(context)
 
-    out_path = context.get_formatted(
-        'fileFormatJsonOut') if 'fileFormatJsonOut' in context else None
-
-    rewriter = ObjectRewriter(context.get_formatted_iterable,
-                              JsonRepresenter())
-    rewriter.files_in_to_out(in_path=in_path, out_path=out_path)
+    ObjectRewriterStep(__name__, 'fileFormatJson', context).run_step(
+        JsonRepresenter())
 
     logger.debug("done")
+
+
+def deprecated(context):
+    """Create new style in params from deprecated."""
+    if 'fileFormatJsonIn' in context:
+        context.assert_keys_have_values(__name__,
+                                        'fileFormatJsonIn',
+                                        'fileFormatJsonOut')
+
+        context['fileFormatJson'] = {'in': context['fileFormatJsonIn'],
+                                     'out': context['fileFormatJsonOut']}
+
+        logger.warning("fileFormatJsonIn and fileFormatJsonOut "
+                       "are deprecated. They will stop working upon the next "
+                       "major release. Use the new context key fileFormatJson "
+                       "instead. It's a lot better, promise! For the moment "
+                       "pypyr is creating the new fileFormatJson key for you "
+                       "under the hood.")
--- tests/unit/pypyr/steps/fileformatjson_test.py
+++ tests/unit/pypyr/steps/fileformatjson_test.py
@@ -1,13 +1,16 @@
 """fileformatjson.py unit tests."""
+import logging
 import os
 import json
+from unittest.mock import patch
+import shutil
 from pypyr.context import Context
 from pypyr.errors import KeyInContextHasNoValueError, KeyNotInContextError
 import pypyr.steps.fileformatjson as fileformat
 import pytest
 
 
-def test_fileformatjson_no_inpath_raises():
+def test_fileformatjson_no_in_obj_raises():
     """None in path raises."""
     context = Context({
         'k1': 'v1'})
@@ -15,67 +18,53 @@ def test_fileformatjson_no_inpath_raises():
     with pytest.raises(KeyNotInContextError) as err_info:
         fileformat.run_step(context)
 
-    assert str(err_info.value) == ("context['fileFormatJsonIn'] "
-                                   "doesn't exist. It must exist for "
-                                   "pypyr.steps.fileformatjson.")
+    assert str(err_info.value) == (
+        "fileFormatJson not found in the pypyr context.")
+
+
+def test_fileformatjson_no_inpath_raises():
+    """None in path raises."""
+    context = Context({
+        'fileFormatJson': 'v1'})
+
+    with pytest.raises(KeyNotInContextError) as err_info:
+        fileformat.run_step(context)
+
+    assert str(err_info.value) == (
+        "context['fileFormatJson']['in'] doesn't exist. It must exist for "
+        "pypyr.steps.fileformatjson.")
 
 
 def test_fileformatjson_empty_inpath_raises():
     """Empty in path raises."""
     context = Context({
-        'fileFormatJsonIn': None})
+        'fileFormatJson': {'in': None}})
 
     with pytest.raises(KeyInContextHasNoValueError) as err_info:
         fileformat.run_step(context)
 
-    assert str(err_info.value) == ("context['fileFormatJsonIn'] must have "
-                                   "a value for pypyr.steps.fileformatjson.")
-
-
-def test_fileformatjson_no_outpath_raises():
-    """None out path raises."""
-    context = Context({
-        'fileFormatJsonIn': 'blah',
-        'k1': 'v1'})
-
-    with pytest.raises(KeyNotInContextError) as err_info:
-        fileformat.run_step(context)
-
-    assert str(err_info.value) == ("context['fileFormatJsonOut'] "
-                                   "doesn't exist. It must exist for "
-                                   "pypyr.steps.fileformatjson.")
-
-
-def test_fileformatjson_empty_outpath_raises():
-    """Empty in path raises."""
-    context = Context({
-        'fileFormatJsonIn': 'blah',
-        'fileFormatJsonOut': None})
-
-    with pytest.raises(KeyInContextHasNoValueError) as err_info:
-        fileformat.run_step(context)
-
-    assert str(err_info.value) == ("context['fileFormatJsonOut'] must have "
+    assert str(err_info.value) == ("context['fileFormatJson']['in'] must have "
                                    "a value for pypyr.steps.fileformatjson.")
 
 
 def test_fileformatjson_pass_no_substitutions():
     """Relative path to file should succeed.
 
-     Strictly speaking not a unit test.
+    Strictly speaking not a unit test.
     """
     context = Context({
         'ok1': 'ov1',
-        'fileFormatJsonIn': './tests/testfiles/test.json',
-        'fileFormatJsonOut': './tests/testfiles/out/out.json'})
+        'fileFormatJson': {'in': './tests/testfiles/test.json',
+                           'out': './tests/testfiles/out/out.json'}})
 
     fileformat.run_step(context)
 
     assert context, "context shouldn't be None"
-    assert len(context) == 3, "context should have 2 items"
+    assert len(context) == 2, "context should have 2 items"
     assert context['ok1'] == 'ov1'
-    assert context['fileFormatJsonIn'] == './tests/testfiles/test.json'
-    assert context['fileFormatJsonOut'] == './tests/testfiles/out/out.json'
+    assert context['fileFormatJson'] == {
+        'in': './tests/testfiles/test.json',
+        'out': './tests/testfiles/out/out.json'}
 
     with open('./tests/testfiles/out/out.json') as outfile:
         outcontents = json.load(outfile)
@@ -89,10 +78,42 @@ def test_fileformatjson_pass_no_substitutions():
     os.remove('./tests/testfiles/out/out.json')
 
 
+def test_fileformatjson_edit_no_substitutions():
+    """Relative path to file should succeed, no out means in place edit.
+
+    Strictly speaking not a unit test.
+    """
+    shutil.copyfile('./tests/testfiles/test.json',
+                    './tests/testfiles/out/edittest.json')
+
+    context = Context({
+        'ok1': 'ov1',
+        'fileFormatJson': {'in': './tests/testfiles/out/edittest.json'}})
+
+    fileformat.run_step(context)
+
+    assert context, "context shouldn't be None"
+    assert len(context) == 2, "context should have 2 items"
+    assert context['ok1'] == 'ov1'
+    assert context['fileFormatJson'] == {
+        'in': './tests/testfiles/out/edittest.json'}
+
+    with open('./tests/testfiles/out/edittest.json') as outfile:
+        outcontents = json.load(outfile)
+
+    assert len(outcontents) == 3
+    assert outcontents['key1'] == "value1"
+    assert outcontents['key2'] == "value2"
+    assert outcontents['key3'] == "value3"
+
+    # atrociously lazy test clean-up
+    os.remove('./tests/testfiles/out/edittest.json')
+
+
 def test_fileformatjson_pass_with_substitutions():
     """Relative path to file should succeed.
 
-     Strictly speaking not a unit test.
+    Strictly speaking not a unit test.
     """
     context = Context({
         'k1': 'v1',
@@ -100,17 +121,17 @@ def test_fileformatjson_pass_with_substitutions():
         'k3': 'v3',
         'k4': 'v4',
         'k5': 'v5',
-        'fileFormatJsonIn': './tests/testfiles/testsubst.json',
-        'fileFormatJsonOut': './tests/testfiles/out/outsubst.json'})
+        'fileFormatJson': {'in': './tests/testfiles/testsubst.json',
+                           'out': './tests/testfiles/out/outsubst.json'}})
 
     fileformat.run_step(context)
 
     assert context, "context shouldn't be None"
-    assert len(context) == 7, "context should have 7 items"
+    assert len(context) == 6, "context should have 6 items"
     assert context['k1'] == 'v1'
-    assert context['fileFormatJsonIn'] == './tests/testfiles/testsubst.json'
-    assert context['fileFormatJsonOut'] == ('./tests/testfiles/out/'
-                                            'outsubst.json')
+    assert context['fileFormatJson'] == {
+        'in': './tests/testfiles/testsubst.json',
+        'out': './tests/testfiles/out/outsubst.json'}
 
     with open('./tests/testfiles/out/outsubst.json') as outfile:
         outcontents = json.load(outfile)
@@ -137,7 +158,56 @@ def test_fileformatjson_pass_with_substitutions():
 def test_fileformatjson_pass_with_path_substitutions():
     """Relative path to file should succeed with path subsitutions.
 
-     Strictly speaking not a unit test.
+    Strictly speaking not a unit test.
+    """
+    context = Context({
+        'k1': 'v1',
+        'k2': 'v2',
+        'k3': 'v3',
+        'k4': 'v4',
+        'k5': 'v5',
+        'pathIn': 'testsubst',
+        'pathOut': 'outsubst',
+        'fileFormatJson': {'in': './tests/testfiles/{pathIn}.json',
+                           'out': './tests/testfiles/out/{pathOut}.json'}})
+
+    fileformat.run_step(context)
+
+    assert context, "context shouldn't be None"
+    assert len(context) == 8, "context should have 8 items"
+    assert context['k1'] == 'v1'
+    assert context['fileFormatJson'] == {
+        'in': './tests/testfiles/{pathIn}.json',
+        'out': './tests/testfiles/out/{pathOut}.json'}
+
+    with open('./tests/testfiles/out/outsubst.json') as outfile:
+        outcontents = json.load(outfile)
+
+    expected = {
+        "key1": "v1value !£$% *",
+        "key2_v2": {
+            "k21": "value",
+            "abc": "v3 def v4",
+            "def": [
+                "l1",
+                "l2 v5",
+                "l3"
+            ]
+        }
+    }
+
+    assert outcontents == expected
+
+    # atrociously lazy test clean-up
+    os.remove('./tests/testfiles/out/outsubst.json')
+
+# --------------------------- deprecated -------------------------------------
+
+
+def test_fileformatjson_pass_with_path_substitutions_deprecated():
+    """Relative path to file should succeed with path subsitutions.
+
+    Strictly speaking not a unit test.
     """
     context = Context({
         'k1': 'v1',
@@ -150,14 +220,27 @@ def test_fileformatjson_pass_with_path_substitutions():
         'fileFormatJsonIn': './tests/testfiles/{pathIn}.json',
         'fileFormatJsonOut': './tests/testfiles/out/{pathOut}.json'})
 
-    fileformat.run_step(context)
+    logger = logging.getLogger('pypyr.steps.fileformatjson')
+    with patch.object(logger, 'warning') as mock_logger_warn:
+        fileformat.run_step(context)
+
+    mock_logger_warn.assert_called_once_with(
+        "fileFormatJsonIn and fileFormatJsonOut are deprecated. "
+        "They will stop "
+        "working upon the next major release. Use the new context key "
+        "fileFormatJson instead. It's a lot better, promise! "
+        "For the moment pypyr "
+        "is creating the new fileFormatJson key for you under the hood.")
 
     assert context, "context shouldn't be None"
-    assert len(context) == 9, "context should have 9 items"
+    assert len(context) == 10, "context should have 10 items"
     assert context['k1'] == 'v1'
     assert context['fileFormatJsonIn'] == './tests/testfiles/{pathIn}.json'
     assert context['fileFormatJsonOut'] == ('./tests/testfiles/out/'
                                             '{pathOut}.json')
+    assert context['fileFormatJson'] == {
+        'in': './tests/testfiles/{pathIn}.json',
+        'out': './tests/testfiles/out/{pathOut}.json'}
 
     with open('./tests/testfiles/out/outsubst.json') as outfile:
         outcontents = json.load(outfile)
@@ -181,6 +264,46 @@ def test_fileformatjson_pass_with_path_substitutions():
     os.remove('./tests/testfiles/out/outsubst.json')
 
 
+def test_fileformatjson_empty_inpath_raises_deprecated():
+    """Empty in path raises."""
+    context = Context({
+        'fileFormatJsonIn': None})
+
+    with pytest.raises(KeyInContextHasNoValueError) as err_info:
+        fileformat.run_step(context)
+
+    assert str(err_info.value) == ("context['fileFormatJsonIn'] must have "
+                                   "a value for pypyr.steps.fileformatjson.")
+
+
+def test_fileformatjson_no_outpath_raises_deprecated():
+    """None out path raises."""
+    context = Context({
+        'fileFormatJsonIn': 'blah',
+        'k1': 'v1'})
+
+    with pytest.raises(KeyNotInContextError) as err_info:
+        fileformat.run_step(context)
+
+    assert str(err_info.value) == ("context['fileFormatJsonOut'] "
+                                   "doesn't exist. It must exist for "
+                                   "pypyr.steps.fileformatjson.")
+
+
+def test_fileformatjson_empty_outpath_raises_deprecated():
+    """Empty in path raises."""
+    context = Context({
+        'fileFormatJsonIn': 'blah',
+        'fileFormatJsonOut': None})
+
+    with pytest.raises(KeyInContextHasNoValueError) as err_info:
+        fileformat.run_step(context)
+
+    assert str(err_info.value) == ("context['fileFormatJsonOut'] must have "
+                                   "a value for pypyr.steps.fileformatjson.")
+# --------------------------- teardown ---------------------------------------
+
+
 def teardown_module(module):
-    """Teardown"""
+    """Teardown."""
     os.rmdir('./tests/testfiles/out/')
