dc89d23c12895c56ab34d8e11e5f018f8b325797
thomas <18520168+yaythomas@users.noreply.github.com>
2020-01-28 21:07:13 +0000

    python 3.8

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ import pypyr.version
 
 here = path.abspath(path.dirname(__file__))
 
-short_description = """pipeline runner command line to run pipelines defined in
-                       yaml"""
+short_description = """task-runner cli to run pipelines defined in yaml"""
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -63,7 +62,8 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
 
     # What does your project relate to?
