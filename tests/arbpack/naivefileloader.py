b632918cf88de3bd03cec9ad405985cf46d743b8
thomas <18520168+yaythomas@users.noreply.github.com>
2022-02-05 22:56:10 +0000

    test loader utf-8
    make test loader always get utf-8 for windows test compat.
    Ref #356.

--- tests/arbpack/naivefileloader.py
+++ tests/arbpack/naivefileloader.py
@@ -8,5 +8,5 @@ working_dir_tests = Path.cwd().joinpath('tests')
 def get_pipeline_definition(pipeline_name, parent):
     """Simplified file loader for integration testing."""
     with open(working_dir_tests.joinpath(
-            f'{pipeline_name}.yaml')) as yaml_file:
+            f'{pipeline_name}.yaml'), encoding='utf-8') as yaml_file:
         return pypyr.yaml.get_pipeline_yaml(yaml_file)
