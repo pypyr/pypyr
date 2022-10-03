a2fcde6fd83afc16ab56e35f808e250ec529a6e9
thomas <18520168+yaythomas@users.noreply.github.com>
2022-10-03 21:07:07 +0100

    log level > warning to stderr
    pypyr previously would output all log entries to stderr.
    stderr is the default for StreamHandler.
    Add separate StreamHandlers for stdout and stderr.
    Now log-levels <= NOTIFY go to stdout, higher than t stderr.

--- conftest.py
+++ conftest.py
@@ -1,14 +1,15 @@
 """Pytest configuration file.
 
 This is a bit of a, ahem, alternative way of allowing testpy to find the pypyr
-module, without having to go into your virtualenv or dealing with setup.py
-first. This is because pytest makes the dir this file is in the root.
+module, without having to go into your virtualenv or dealing with pip install
+first. This is because pytest makes the dir this file is in the root & adds it
+to sys.paths for you.
+
+If you remove this file, pytest will fail UNLESS you `pip install -e .[dev]`.
+
+Keeping this file means you can run pytest in an environment with all the
+other dependencies installed but not pypyr itself.
 
 What this file is actually for is per-directory fixture scopes:
 http://doc.pytest.org/en/latest/example/simple.html#package-directory-level-fixtures-setups
 """
-import logging
-
-from pypyr.log.logger import set_root_logger
-
-set_root_logger(logging.DEBUG)
