1166d70e490427d27909b40f699905e212c86cbe
thomas <18520168+yaythomas@users.noreply.github.com>
2023-03-12 18:25:53 -0700

    Bump version: 5.7.1 → 5.8.0

--- pypyr/__init__.py
+++ pypyr/__init__.py
@@ -5,6 +5,6 @@ NOTIFY (25) log level and notify() method to the global logger object.
 """
 import pypyr.log.logger
 
-__version__ = "5.7.1"
+__version__ = "5.8.0"
 
 pypyr.log.logger.set_up_notify_log_level()
