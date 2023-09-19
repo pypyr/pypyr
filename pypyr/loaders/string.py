f608e0940c71a0286ddbdff4e7e156a260f7b83b
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-09-19 15:42:43 -0300

    Add String loader

---
+++ pypyr/loaders/string.py
@@ -0,0 +1,33 @@
+"""Load pipelines from string."""
+import logging
+
+from pypyr.pipedef import PipelineDefinition, PipelineFileInfo
+from pypyr.yaml import get_pipeline_yaml
+
+logger = logging.getLogger(__name__)
+
+
+def get_pipeline_definition(pipeline_name, parent=None):
+    """
+    Receives a pipeline as string, parses it and returns a PipelineDefinition.
+
+    Args:
+        pipeline_name (str): pipeline as string.
+        parent (Path-like): ignored
+
+    Returns:
+        PipelineDefinition describing the pipeline.
+            The dict parsed from the pipeline is in its .pipeline property.
+    """
+    logger.debug("starting")
+
+    pipeline = get_pipeline_yaml(pipeline_name)
+    info = PipelineFileInfo(
+        pipeline_name="", parent=parent, loader=__name__, path=None
+    )
+    definition = PipelineDefinition(pipeline=pipeline, info=info)
+
+    logger.debug("found %d stages in pipeline.", len(definition.pipeline))
+    logger.debug("done")
+
+    return definition
---
+++ tests/unit/pypyr/loaders/string__test.py
@@ -0,0 +1,25 @@
+"""pypyr.loaders.string unit tests."""
+from unittest.mock import patch
+
+import pypyr.loaders.string as string_loader
+from pypyr.pipedef import PipelineDefinition, PipelineFileInfo
+
+
+@patch("ruamel.yaml.YAML.load", return_value="mocked pipeline def")
+def test_get_pipeline_definition(mocked_yaml):
+    """Unit test get_pipeline_definition."""
+    pipeline = "pipeline"
+
+    pipeline_def = string_loader.get_pipeline_definition(pipeline, None)
+
+    mocked_yaml.assert_called_once_with(pipeline)
+    expected_pipeline_def = PipelineDefinition(
+        "mocked pipeline def",
+        PipelineFileInfo(
+            pipeline_name="",
+            loader="pypyr.loaders.string",
+            parent=None,
+            path=None,
+        ),
+    )
+    assert pipeline_def == expected_pipeline_def
